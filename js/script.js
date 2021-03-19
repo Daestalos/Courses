dacead3b223a4812eec08915b8adf6ba07a834b9
Alex Bordyug <alex.bordyug.daes@gmail.com>
2021-03-19 15:42:00 +0300

    L11 practice begin

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?"),
+    objMovies = {},
+    objActors = {},
+    dataEmpty = [],
+    questionFilm = prompt("Один из просмотренный фильмов?");
+    question
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: objMovies,
+    actors: objActors,
+    genres: dataEmpty,
+    privat: false
+};
+
