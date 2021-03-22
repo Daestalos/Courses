a690609eb15a83f18bc8a0220c2c887d4f8fc012
Alex Bordyug <alex.bordyug.daes@gmail.com>
2021-03-22 13:55:57 +0300

    New folder, L12 Practice1 done.

---
+++ Practice/L12 Practice №1.js
@@ -0,0 +1,40 @@
+"use strict";
+
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", ''),
+    objActors = {},
+    objMovies = {},
+    dataEmpty = [],
+    questionFilm = prompt("Один из просмотренный фильмов?", ''),
+    questionRating = prompt("На сколько оцените его?", '');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: objMovies,
+    actors: objActors,
+    genres: dataEmpty,
+    privat: false
+};
+personalMovieDB.movies[questionFilm] = questionRating;
+console.log(personalMovieDB);
+
+
+
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// const a = prompt("Один из просмотренный фильмов?", ''),
+//       b = prompt("На сколько оцените его?", ''),
+//       c = prompt("Один из просмотренный фильмов?", ''),
+//       d = prompt("На сколько оцените его?", '');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
--- js/script.js
+++ js/script.js
@@ -1,17 +1,1 @@
 "use strict";
-
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?"),
-    objMovies = {},
-    objActors = {},
-    dataEmpty = [],
-    questionFilm = prompt("Один из просмотренный фильмов?");
-    question
-
-let personalMovieDB = {
-    count: numberOfFilms,
-    movies: objMovies,
-    actors: objActors,
-    genres: dataEmpty,
-    privat: false
-};
-
