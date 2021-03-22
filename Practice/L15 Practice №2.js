76f3cac8987119211af4b3091d0bf506bdace3f2
Alex Bordyug <alex.bordyug.daes@gmail.com>
2021-03-22 15:59:21 +0300

    L15 Practice2 done. work with cycles

---
+++ Practice/L15 Practice №2.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+    // с циклом if
+
+// for (let i = 0; i < 2; i++)
+// {
+//     const a = prompt("Один из просмотренный фильмов?", ''),
+//           b = prompt("На сколько оцените его?", '');
+
+//     if (a != null && b != null && a!='' && b!= '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done.');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }    
+// }
+
+    // с циклом while
+
+for (let i = 0; i < 2; i++)
+{
+    const a = prompt("Один из просмотренный фильмов?", ''),
+          b = prompt("На сколько оцените его?", '');
+
+    while (a != null && b != null && a!='' && b!= '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done.');
+        break;
+    }
+    console.log('here.');
+ 
+}
+
+
+if (personalMovieDB.count <10){
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <30 ){
+    alert('Вы классический зритель');
+} else if (personalMovieDB >= 30) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
+console.log(personalMovieDB);
